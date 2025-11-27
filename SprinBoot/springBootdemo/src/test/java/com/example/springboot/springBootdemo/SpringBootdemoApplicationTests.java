e28b39c078c099a1f0963172228629169cccab45
Ayush kumar <115687622+Ayu18z@users.noreply.github.com>
2025-11-27 15:00:41 +0530

    Add files via upload

---
+++ SprinBoot/springBootdemo/src/test/java/com/example/springboot/springBootdemo/SpringBootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.springBootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
